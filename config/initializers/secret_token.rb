3c1716e2c50d5f8e4b1a1252d8d56a98b25c77f4
Casper Fabricius <me@casperfabricius.com>
2011-03-20 11:37:27 -0700

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkQuest::Application.config.secret_token = 'c88efef5260ee98288f5239f9d2cf94a48cc80cea4607fc573f7c5697b96de951ef3568a74b47a3267a9c46e05673974f4e5643a4933c0a864a1807c94046214'
